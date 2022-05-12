6bb9fe12c32ee6029ef7646cd8f297d56700e8f9
nikola8217 <gileer404@gmail.com>
2022-05-12 12:41:40 +0200

    Router

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import Card from "../components/share/Card"
+import {Link} from 'react-router-dom'
+
+function AboutPage() {
+  return (
+    <Card>
+        <h1>About This Project</h1>
+        <p>This is a React app to leave feedback for a product or servise</p>
+        <p>Version: 1.0.0</p>
+
+        <p>
+            <Link to='/'>Back To Home</Link>
+            
+        </p>
+    </Card>
+  )
+}
+
+export default AboutPage
